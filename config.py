d9943fce982b79ff65ea2d45fa271f94e3aba2a4
pro-extremecoder <vladislavmustyatsa@gmail.com>
2020-03-19 20:55:40 +0200

    Revert "Set up app, SQlAlchemy,Migrate"
    This reverts commit 5776c96ebb45cc39958419eb2938b34f941ef5fd.

--- config.py
+++
@@ -1,5 +0,0 @@
-class Configuration:
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:1@localhost/project"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '1'
