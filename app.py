3321ac8815bfc813f7883bbe52a25d679005c4e8
pro-extremecoder <vladislavmustyatsa@gmail.com>
2020-03-19 20:59:35 +0200

    Revert "Revert "Set up app, SQlAlchemy,Migrate""
    This reverts commit d9943fce982b79ff65ea2d45fa271f94e3aba2a4.

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_security import Security, SQLAlchemyUserDatastore
+from config import Configuration
+
+
+# App
+app = Flask(__name__)
+app.config.from_object(Configuration)
+
+# DateBase
+db = SQLAlchemy(app)
+
+# Migration
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
