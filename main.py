aabb18a9163ea317cd16b6e6c8d864378bbb5a20
pro-extremecoder <vladislavmustyatsa@gmail.com>
2020-08-07 12:47:23 +0300

    After adding checking if MAIL_PASSWORD was entered

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ from app import app
 
 
 if __name__ == "__main__":
-	app.run()
+	if app.config['MAIL_PASSWORD']:
+		app.run()
+	else:
+		print(f'MAIL_PASSWORD={app.config["MAIL_PASSWORD"]}\nEnter MAIL_PASSWORD as environment variable')
